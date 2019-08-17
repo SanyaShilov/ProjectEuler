6bde8289091a9a0bdf45792723347d7385307c16
Alexandr Shilov <sasha1998lost@yandex.ru>
2019-08-17 11:27:00 +0300

    solved problem 78

--- python/problem78.py
+++ python/problem78.py
@@ -1,55 +1,27 @@
 import array
 
 
-ANSWER =
+ANSWER = 55374
+DIVISOR = 10 ** 6
 
 
-l = 101
-
-#more than 30000
-
-
-
-lst = [[0 for i in range(l)] for j in range(l)]
-lst[0][0] = 1
-for i in range(1, l):
-    for j in range(i):
-        lst[i][j] = sum(lst[i-j-1][:j+1])
-
-l = 101
-
-r = range(l+1)
-
-summed = [array.array('L', (1,)) for i in r]
-
-def gsum (i, j):
-    global lst, summed
-    ind = i-j-1
-    if j >= ind:
-        return summed[ind][-1]
-    if j == 0:
-        return 1
-    temp = (summed[ind][j-1]+lst[ind][j])%1000000
-    summed[ind].append(temp)
-    return temp
+def main():
+    lst = [array.array('L', (1,))]
+    i = 0
+    while True:
+        i += 1
+        new_row = array.array('L', (0 for _ in range(i)))
+        i_j_1 = i
+        for j in range(i):
+            i_j_1 -= 1
+            new_row[j] = lst[i_j_1][0]
+            if len(lst[i_j_1]) > 1:
+                lst[i_j_1][1] = (lst[i_j_1][0] + lst[i_j_1][1]) % DIVISOR
+                lst[i_j_1].pop(0)
+        if sum(new_row) % DIVISOR == 0:
+            return i
+        lst.append(new_row)
 
 
-lst = [array.array('L') for i in r]
-print('done')
-lst[0].append(1)
-i = 1
-for i in range(1, l+1):
-    result = 0
-    for j in range(i):
-        temp = gsum(i, j)
-        result += temp
-        lst[i].append(temp)
-    if not result % 1000000:
-        print(i)
-        break
-    if not i % 1000:
-        print(i)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(main())
