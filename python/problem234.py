40f37d7b685c77fc4e7fa6630bf7e204048b7868
Alexandr Shilov <sasha1998lost@yandex.ru>
2019-08-19 18:19:46 +0300

    solved problem 234

--- python/problem234.py
+++ python/problem234.py
@@ -1,31 +1,37 @@
+import itertools
+
 import euler
 
-prime_list = euler.prime_list(1000)
+
+ANSWER = 1259187438574927161
+LIMIT = 999966663333
 
 
-
-def semidiv (n, a, b):
-    if n % a:
-        return not n % b
-    return n % b
-
-i1 = 0
-i2 = 0
-pr1 = prime_list[i1]
-pr2 = prime_list[i2]
-s = 0
-for i in range(4, 1000):
-    sq = i**0.5
-    pr1n = prime_list[i1+1]
-    if pr1n <= sq:
-        pr1 = pr1n
-        i1 += 1
-    if pr2 < sq:
-        i2 += 1
-        pr2 = prime_list[i2]
-    if semidiv(i, pr1, pr2):
-        s += i
-print(s)
+def main():
+    total = 0
+    prime_list = euler.prime_list(int(LIMIT ** 0.5) + 100)
+    for i, prime in enumerate(prime_list):
+        upper_limit = (
+            prime_list[i + 1] if i != len(prime_list) - 1 else prime
+        ) ** 2
+        lower_limit = (
+            prime_list[i - 1] if i != 0 else prime
+        ) ** 2
+        for n in itertools.count(prime + 1):
+            product = prime * n
+            if product >= upper_limit or product >= LIMIT:
+                break
+            if n == prime_list[i + 1]:
+                continue
+            total += product
+        for n in range(prime - 1, -1, -1):
+            product = prime * n
+            if product <= lower_limit:
+                break
+            if n == prime_list[i - 1] or product > LIMIT:
+                continue
+            total += product
+    return total
 
 
 if __name__ == '__main__':
