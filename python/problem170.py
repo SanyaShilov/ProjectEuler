7f9b608dc0deba46e5fdeb8f7d9df7e678fca680
Alexandr Shilov <sasha1998lost@yandex.ru>
2019-08-31 17:35:02 +0300

    solved problem 170

--- python/problem170.py
+++ python/problem170.py
@@ -1,23 +1,50 @@
-ANSWER =
+import itertools
+
+import euler
 
 
-digits = '0123456789'
+ANSWER = 9857164023
+DIVISORS_DICT = {}
 
-def form_next_pandigitals (last):
-    for n in last:
-        for d in digits:
-            if d not in n:
-                yield n + d
 
-last = [d for d in digits if d != '0']
-al = []
-al.extend(last)
-for i in range(9):
-    nxt = list(form_next_pandigitals(last))
-    print(len(nxt))
-    last = nxt
-    al.extend(nxt)
-ial = [int(a) for a in al]
+def number(permutation):
+    result = 0
+    for digit in permutation:
+        result = result * 10 + digit
+    return result
+
+
+def is_pandigital(a, b, n):
+    str_abn = str(a) + str(b) + str(n)
+    return len(str_abn) == len(set(str_abn)) == 10
+
+
+def divisors(num):
+    if num not in DIVISORS_DICT:
+        DIVISORS_DICT[num] = euler.divisors(num)
+    return DIVISORS_DICT[num]
+
+
+def can_be_formed(permutation):
+    for i in range(1, 10):
+        if permutation[i] != 0:
+            a, b = number(permutation[:i]), number(permutation[i:])
+            div_a, div_b = divisors(a), divisors(b)
+            intersection = div_a.intersection(div_b)
+            intersection.discard(1)
+            for n in intersection:
+                a_n, b_n = a // n, b // n
+                if is_pandigital(a_n, b_n, n):
+                    return True
+    return False
+
+
+def main():
+    return next(
+        number(permutation)
+        for permutation in itertools.permutations(reversed(range(10)))
+        if can_be_formed(permutation)
+    )
 
 
 if __name__ == '__main__':
